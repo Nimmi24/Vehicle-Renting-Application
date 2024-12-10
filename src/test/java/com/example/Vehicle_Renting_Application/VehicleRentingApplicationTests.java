5afe7258690bd18ab9ed05ed779342226d3c7e52
Nimmi24 <93963505+Nimmi24@users.noreply.github.com>
2024-12-10 15:50:00 +0530

    intitalising the project

---
+++ src/test/java/com/example/Vehicle_Renting_Application/VehicleRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vehicle_Renting_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
