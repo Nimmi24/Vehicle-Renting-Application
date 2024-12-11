c892c8e799ecb758cf3653306285936cb7257c14
Nimmi24 <93963505+Nimmi24@users.noreply.github.com>
2024-12-11 14:26:34 +0530

    configuring the image

--- src/main/java/com/example/Vehicle_Renting_Application/Repository/ImageRepository.java
+++ src/main/java/com/example/Vehicle_Renting_Application/Repository/ImageRepository.java
@@ -1,5 +1,9 @@
 package com.example.Vehicle_Renting_Application.Repository;
 
-public interface ImageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Vehicle_Renting_Application.entity.Image;
+
+public interface ImageRepository extends JpaRepository<Image , Integer>{
 
 }
