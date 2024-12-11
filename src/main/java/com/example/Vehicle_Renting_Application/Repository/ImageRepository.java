5819b8bf8e012c97386841e99fba27413975c536
Nimmi24 <93963505+Nimmi24@users.noreply.github.com>
2024-12-11 14:25:11 +0530

    Image

---
+++ src/main/java/com/example/Vehicle_Renting_Application/Repository/ImageRepository.java
@@ -0,0 +1,5 @@
+package com.example.Vehicle_Renting_Application.Repository;
+
+public interface ImageRepository {
+
+}
---
+++ src/main/java/com/example/Vehicle_Renting_Application/entity/Image.java
@@ -0,0 +1,5 @@
+package com.example.Vehicle_Renting_Application.entity;
+
+public class Image {
+
+}
